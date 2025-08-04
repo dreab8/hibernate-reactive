712598e6b9fab96c16abb70e07df942f30d64b4d
Andrea Boriero <andrea@hibernate.org>
2025-08-04 11:08:53 +0200

    Upgrade Hibernate ORM to 7.1.0.CR2

--- hibernate-reactive-core/src/main/java/org/hibernate/reactive/query/sqm/mutation/internal/cte/ReactiveCteDeleteHandler.java
+++ hibernate-reactive-core/src/main/java/org/hibernate/reactive/query/sqm/mutation/internal/cte/ReactiveCteDeleteHandler.java
@@ -5,11 +5,11 @@
  */
 package org.hibernate.reactive.query.sqm.mutation.internal.cte;
 
-import java.lang.invoke.MethodHandles;
 import java.util.List;
 import java.util.Map;
 
 import org.hibernate.engine.spi.SessionFactoryImplementor;
+import org.hibernate.internal.util.MutableObject;
 import org.hibernate.query.spi.DomainQueryExecutionContext;
 import org.hibernate.query.sqm.internal.DomainParameterXref;
 import org.hibernate.query.sqm.mutation.internal.MultiTableSqmMutationConverter;
@@ -17,28 +17,28 @@ import org.hibernate.query.sqm.mutation.internal.cte.CteDeleteHandler;
 import org.hibernate.query.sqm.mutation.internal.cte.CteMutationStrategy;
 import org.hibernate.query.sqm.tree.delete.SqmDeleteStatement;
 import org.hibernate.query.sqm.tree.expression.SqmParameter;
-import org.hibernate.reactive.logging.impl.Log;
-import org.hibernate.reactive.logging.impl.LoggerFactory;
 import org.hibernate.sql.ast.tree.cte.CteContainer;
 import org.hibernate.sql.ast.tree.cte.CteStatement;
 import org.hibernate.sql.ast.tree.cte.CteTable;
 import org.hibernate.sql.ast.tree.expression.Expression;
 import org.hibernate.sql.ast.tree.expression.JdbcParameter;
+import org.hibernate.sql.exec.spi.JdbcOperationQuerySelect;
+import org.hibernate.sql.exec.spi.JdbcParameterBindings;
 
 /**
  * @see org.hibernate.query.sqm.mutation.internal.cte.CteDeleteHandler
  */
 public class ReactiveCteDeleteHandler extends CteDeleteHandler implements ReactiveAbstractCteMutationHandler {
 
-	private static final Log LOG = LoggerFactory.make( Log.class, MethodHandles.lookup() );
-
 	protected ReactiveCteDeleteHandler(
 			CteTable cteTable,
 			SqmDeleteStatement<?> sqmDeleteStatement,
 			DomainParameterXref domainParameterXref,
 			CteMutationStrategy strategy,
-			SessionFactoryImplementor sessionFactory) {
-		super( cteTable, sqmDeleteStatement, domainParameterXref, strategy, sessionFactory );
+			SessionFactoryImplementor sessionFactory,
+			DomainQueryExecutionContext context,
+			MutableObject<JdbcParameterBindings> firstJdbcParameterBindingsConsumer) {
+		super( cteTable, sqmDeleteStatement, domainParameterXref, strategy, sessionFactory, context, firstJdbcParameterBindingsConsumer );
 	}
 
 	@Override
@@ -52,13 +52,13 @@ public class ReactiveCteDeleteHandler extends CteDeleteHandler implements Reacti
 	}
 
 	@Override
-	public int execute(DomainQueryExecutionContext executionContext) {
-		throw LOG.nonReactiveMethodCall( "reactiveExecute" );
+	public Expression createCountStar(SessionFactoryImplementor factory, MultiTableSqmMutationConverter sqmConverter) {
+		return super.createCountStar( factory, sqmConverter );
 	}
 
 	@Override
-	public Expression createCountStar(SessionFactoryImplementor factory, MultiTableSqmMutationConverter sqmConverter) {
-		return super.createCountStar( factory, sqmConverter );
+	public JdbcOperationQuerySelect getSelect() {
+		return super.getSelect();
 	}
 
 }
