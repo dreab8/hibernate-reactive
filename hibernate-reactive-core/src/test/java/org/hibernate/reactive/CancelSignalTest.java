aafdc3001005263d42ba3aee6de5f9c4783372b6
Davide D'Alto <davide@hibernate.org>
2025-10-17 11:18:54 +0200

    Disable CancelSignalTest

--- hibernate-reactive-core/src/test/java/org/hibernate/reactive/CancelSignalTest.java
+++ hibernate-reactive-core/src/test/java/org/hibernate/reactive/CancelSignalTest.java
@@ -16,6 +16,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.stream.IntStream;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import org.jboss.logging.Logger;
@@ -33,6 +34,7 @@ import static java.util.concurrent.CompletableFuture.runAsync;
 import static java.util.stream.Stream.concat;
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Disabled
 public class CancelSignalTest extends BaseReactiveTest {
 	private static final Logger LOG = Logger.getLogger( CancelSignalTest.class );
 
@@ -50,7 +52,7 @@ public class CancelSignalTest extends BaseReactiveTest {
 
 		ExecutorService withSessionExecutor = Executors.newFixedThreadPool( executeSize );
 		// Create some jobs that are going to be cancelled asynchronously
-		CompletableFuture[] withSessionFutures = IntStream
+		CompletableFuture<?>[] withSessionFutures = IntStream
 				.range( 0, executeSize )
 				.mapToObj( i -> runAsync(
 						() -> {
@@ -76,7 +78,7 @@ public class CancelSignalTest extends BaseReactiveTest {
 
 		// Create jobs that are going to cancel the previous ones
 		ExecutorService cancelExecutor = Executors.newFixedThreadPool( executeSize );
-		CompletableFuture[] cancelFutures = IntStream
+		CompletableFuture<?>[] cancelFutures = IntStream
 				.range( 0, executeSize )
 				.mapToObj( i -> runAsync(
 						() -> {
